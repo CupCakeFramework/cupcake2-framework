3b1ae7843401fd68a9b75257553c54fdf920a0e9
Ricardo Fiorani <ricardo.fiorani@gmail.com>
2014-08-11 20:23:38 -0300

    kickstart

---
+++ frameworkController.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Este arquivo no momento não é utilizado
+ * Porém num futuro próximo aqui terá os controles do próprio framework;
+ * Por ex: Páginas padrões de erro (404 - Não encontrado, 500 - Erro no banco, etc...), 
+ * páginas de agradecimento (Contato Sucesso, Contato Erro, Envio Sucesso, etc...)
+ */
+?>
---
+++ inicializarComponentes.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Este arquivo serve para inicializar os componentes utilizados no site
+ * deixar somente o que for realmente utilizado para não deixar lerdo tanto
+ * o site quanto o servidor.
+ */
+
+/* Exemplo  */
+require_once "app/componentes/Carrinho.php";
+require_once "app/componentes/CupMailer.php";
